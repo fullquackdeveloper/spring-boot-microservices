31dbd9e7c7cc32e346ac9faf344c02ffa0c7ad99
fullquackdeveloper <fullquackdeveloper@gmail.com>
2022-07-09 11:17:17 +0800

    return application name to application yml

--- consumer-service/src/main/java/com/fullquackdeveloper/consumerservice/web/GreetingController.java
+++ consumer-service/src/main/java/com/fullquackdeveloper/consumerservice/web/GreetingController.java
@@ -2,6 +2,8 @@ package com.fullquackdeveloper.consumerservice.web;
 
 import com.fullquackdeveloper.consumerservice.client.HelloClient;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class GreetingController {
 
+    @Autowired
+    Environment environment;
+
     private final HelloClient helloClient;
     @GetMapping("/{name}")
     public String greetHello (@PathVariable String name)
     {
-        return helloClient.sayHello(name);
+        String port = environment.getProperty("local.server.port");
+        return helloClient.sayHello(name) + " from " + port;
     }
 }
